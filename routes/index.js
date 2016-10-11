3a1328ca30b72ee8157e0f3cf51d25bfe4f75ddb
200275643 <chandrareddy.mec@gmail.com>
2016-10-11 19:11:34 -0400

    added changes

--- routes/index.js
+++ routes/index.js
@@ -6,19 +6,22 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: '' });
 });
 
-
+/* GET About page. */
 router.get('/about', function(req, res, next) {
   res.render('about', {title: 'About'});
 });
 
+/* GET Projects page. */
 router.get('/projects', function(req, res, next) {
   res.render('projects', {title:'Projects'});
 });
 
+/* GET Contact page. */
 router.get('/contact', function(req, res, next) {
   res.render('contact', {title:'Contact'});
 });
 
+/* GET Services page. */
 router.get('/services', function(req, res, next) {
   res.render('services', {title: 'Services'});
 });
